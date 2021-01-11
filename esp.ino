e5023a1db70cb3220d54d3ef91def0eec70a4f8b
Kian-Wee <45945080+Kian-Wee@users.noreply.github.com>
2021-01-11 17:40:08 +0800

    Update esp.ino

--- esp.ino
+++ esp.ino
@@ -1,57 +1,32 @@
-//const char* ssid = "";
-//const char* password = "";
-
-
-/*
-//esp32 setup--------------------------------------------
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 
-void setupwifi(){
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-    
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-*/
-
-/*
-//esp8266 setup--------------------------------------------
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-ESP8266WiFiMulti WiFiMulti;
-
-void setup8266() {
-    Serial.begin(115200);
-    delay(10);
-
-    // We start by connecting to a WiFi network
-    WiFi.mode(WIFI_STA);
-    WiFiMulti.addAP(ssid, password);
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Wait for WiFi... ");
-
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
+void setupwifi(const char* ssid,const char* password){  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
-}*/
+  }
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP()); 
+
+}
+
+#include "time.h"
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+}
